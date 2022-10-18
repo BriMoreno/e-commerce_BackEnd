08a3a381c837a13e6aaf5175c0858e82ad4b1fbc
BriMoreno <BriMoreno232@gmail.com>
2022-10-17 21:51:52 -0700

    added the api routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,42 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll()
+    .then((ecommerceDb) => res.json(ecommerceDb))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [{
+      model: Category,
+      attributes: ['category_name']
+    },
+    {
+      model: Tag,
+      attributes: ['tag_name']
+    }
+  ]
+  })
+  .then(productData => {
+    if(!productData){
+      res.status(404).json({ message:'Id not found'});
+    }
+    res.json(productData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
@@ -91,6 +121,16 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((productTagIds) => res.status(200).json(productTagIds))
+  .catch((err) =>{
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 module.exports = router;
